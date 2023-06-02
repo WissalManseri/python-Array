5522e39b0c4d02749caf375a91fa770c4ed03830
WISSAL MN <135167709+WissalManseri@users.noreply.github.com>
2023-06-02 17:28:12 +0100

    Add files via upload

---
+++ array.py
@@ -0,0 +1,21 @@
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+w = cars[0]
+print(w)
+
+cars[0] = "Toyota"
+print(cars)
+
+y = len(cars)
+print(y)
+
+for x in cars:
+  print(x)
+  
+cars.append("Honda")
+print(cars)
+
+cars.append("Honda")
+print(cars)
+
